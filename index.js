c1f685a949cb1c313547a94ef9e70697cb0e8b51
Jehanzeb Siddiqui <100095101+Jehanzeb11@users.noreply.github.com>
2023-10-07 20:51:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const formData = new FormData();
+
+formData.append('imagesList', null)
+
+image.forEach((file) => {
+  formData.append('postFile', {
+    name: file?.path,
+    size: file?.size,
+    type: file?.mime,
+    uri: file?.path
+  });
+});
+  let res = await fetch(
+    `${baseUrl}/fame/saveImage`,
+    {
+      method: 'POST',
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+  );
+  let responseJson = await res.json();
+  console.log("response ======= ",responseJson)
+
+
+  console.log(formData)
